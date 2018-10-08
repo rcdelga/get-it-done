6144ac3df6d4a50c3ad58ebd97aab7c64b4489bc
Rob Delgado <triptix@hotmail.com>
2018-10-07 21:23:31 -0500

    Completed Get-It-Done parts 5 and 6

--- main.py
+++ main.py
@@ -1,32 +1,99 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:lc101@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
-
 db = SQLAlchemy(app)
 
 class Task(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120))
+	completed = db.Column(db.Boolean)
 
 	def __init__(self, name):
 		self.name = name
+		self.completed = False
 
-tasks = []
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(120), unique=True)
+	password = db.Column(db.String(120))
+
+	def __init__(self, email, password):
+		self.email = email
+		self.password = password
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		user = User.query.filter_by(email=email).first()
+		if user and user.password == password:
+			# TODO - "remember" that the user has logged in
+			return redirect('/')
+		else:
+			# TODO - explain why login failed
+			return "<h1>Error!</h1>"
+	return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		verify = request.form['verify']
+
+		# TODO - validate user's data
+
+		existing_user = User.query.filter_by(email=email).first()
+		if not existing_user:
+			new_user = User(email,password)
+			db.session.add(new_user)
+			db.session.commit()
+			# TODO - "remember" the user
+			return redirect('/')
+		else:
+			# TODO - user better response message
+			return "<h1>Duplicate User</h1>"
+
+	return render_template('register.html')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    return render_template('todos.html', title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+
+    return render_template('todos.html',title="Get It Done!",tasks=tasks,completed_tasks=completed_tasks)
+
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+	task_id = int(request.form['task-id'])
+	task = Task.query.get(task_id)
+	task.completed = True
+	db.session.add(task)
+	db.session.commit()
+
+	return redirect('/')
+
 
 if __name__ == '__main__':
 	app.run()
