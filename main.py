49c929fb79f617e72dcde5ce7c2bb8f50e311b38
Rob Delgado <triptix@hotmail.com>
2018-10-08 01:48:19 -0500

    Completed Get-It-Done Part 9

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,27 +6,37 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:lc101@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'r0b84'
 
 class Task(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120))
 	completed = db.Column(db.Boolean)
+	owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-	def __init__(self, name):
+	def __init__(self, name, owner):
 		self.name = name
 		self.completed = False
+		self.owner = owner
 
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(120), unique=True)
 	password = db.Column(db.String(120))
+	tasks = db.relationship('Task', backref='owner')
 
 	def __init__(self, email, password):
 		self.email = email
 		self.password = password
 
+
+@app.before_request
+def require_login():
+	allowed_routes = ['login', 'register']
+	if request.endpoint not in allowed_routes and 'email' not in session:
+		return redirect('/login')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -36,11 +46,11 @@ def login():
 		password = request.form['password']
 		user = User.query.filter_by(email=email).first()
 		if user and user.password == password:
-			# TODO - "remember" that the user has logged in
+			session['email'] = email
+			flash("Logged In")
 			return redirect('/')
 		else:
-			# TODO - explain why login failed
-			return "<h1>Error!</h1>"
+			flash('User password incorrect, or user does not exist.','error')
 	return render_template('login.html')
 
 
@@ -59,7 +69,7 @@ def register():
 			new_user = User(email,password)
 			db.session.add(new_user)
 			db.session.commit()
-			# TODO - "remember" the user
+			session['email'] = email
 			return redirect('/')
 		else:
 			# TODO - user better response message
@@ -67,18 +77,24 @@ def register():
 
 	return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+	del session['email']
+	return redirect('/')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Task(task_name,owner)
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    tasks = Task.query.filter_by(completed=False,owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
 
     return render_template('todos.html',title="Get It Done!",tasks=tasks,completed_tasks=completed_tasks)
 
